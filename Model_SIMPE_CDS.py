89eca1642973d13292565697d7865634f14d0006
Bismarck Gomes Souza Jr <bismarckgomes@gmail.com>
2014-04-16 15:27:19 -0300

    Modelo SIMPLE UDS

--- Model_SIMPE_CDS.py
+++ Model_SIMPE_CDS.py
@@ -15,7 +15,7 @@ class Model_SIMPLE_CDS():
     o modelo SIMPLE para o acoplamento pressão velocidade, e o
     esquema CDS Central Discretization Scheme para interpolar a velocidade."""
 
-    def build_v_matrix(self, grid):
+    def build_matrix_v(self, grid):
         "Classe que constrói a matrz de v* (previsão de velocidade)"
         cpoints = len(grid.cells)
         v_matrix = np.zeros((cpoints, cpoints))
@@ -40,23 +40,38 @@ class Model_SIMPLE_CDS():
             ac_vr = 0.5*A_r*rho_r*v_r
             ad_vr = (A_r*mu_r)/dx_r
 
-            A_vl = - ac_vl - ad_vl
-            A_vc = ac_vr + ad_vr - ac_vl + ad_vl
-            A_vr = ac_vr - ad_vr
+            a_vl = - ac_vl - ad_vl
+            a_vc = ac_vr + ad_vr - ac_vl + ad_vl
+            a_vr = ac_vr - ad_vr
+
+            A_lh = self.center_scheme(grid.A(i-1), grid.A(i))
+            A_rh = self.center_scheme(grid.A(i), grid.A(i+1))
 
             #Filling Matrix
             if i == 0:
-                v_matrix[i][i] = A_vc
-                v_matrix[i][i+1] = A_vr
+                v_matrix[i][i] = a_vc - a_vl*A_lh/A_rh
+                v_matrix[i][i+1] = a_vr
             else:
                 if i == cpoints-1:
-                    v_matrix[i][i-1] = A_vl
-                    v_matrix[i][i] = A_vc
+                    v_matrix[i][i-1] = a_vl
+                    v_matrix[i][i] = a_vc - a_vr*A_rh/A_lh
                 else:
-                    v_matrix[i][i-1] = A_vl
-                    v_matrix[i][i] = A_vc
-                    v_matrix[i][i+1] = A_vr
+                    v_matrix[i][i-1] = a_vl
+                    v_matrix[i][i] = a_vc
+                    v_matrix[i][i+1] = a_vr
+
         return v_matrix
+
+    def build_coef_vector_v(self, grid):
+        #classe que constrói o vetor que será resolvido pelo solver
+        n = len(grid.cells)
+        b = np.zeros(n)
+
+        for i in range(n-1):
+            A_c = self.center_scheme(grid.A(i), grid.A(i+1))
+            b[i] = -A_c*(grid.p(i+1)-grid.p(i))
+
+        return b
 
     #Funções de Interpolação
     def center_scheme(self, leftprop, rightprop):
@@ -67,14 +82,17 @@ if __name__ == '__main__':
 
     from GridFluid import *
 
-    gridteste = GridFluid(100, 500, 0)
+    gridteste = GridFluid(10, 0, 1)
     for i in range(5):
-        gridteste.add_cell(10e-3, 1000, 0.1, 100, 1, 2)
+        A = 0.5-0.05*(i+1)
+        gridteste.add_cell(A=A, dx=0.5, rho=1, mu=0, v_r=1/A, p=10-2.5*(i+1))
 
     modelCD = Model_SIMPLE_CDS()
 
-    A = modelCD.build_v_matrix(gridteste)
-    print A
+    A = modelCD.build_matrix_v(gridteste)
+    b = modelCD.build_coef_vector_v(gridteste)
+    print "\nA:\n", A
+    print "\nb:\n", b
+    print "\nv:\n", (np.matrix(A).I*np.matrix(b).T).A1
     print
 
-    print "TESTE"
---
+++ Model_SIMPE_UDS.py
@@ -0,0 +1,127 @@
+# -*- coding: iso-8859-1 -*-
+"""
+@author: Wagner Queiroz Barros
+@date:   Tue Apr 15 10:55:33 2014
+@email:  wagnerqb@gmail.com
+@brief:  Class used to discretize Navier-Stokes using SIMPLE Method, CDS for P
+and CDS for v. 
+"""
+
+from __future__ import division
+import numpy as np
+
+class Model_SIMPLE_UDS():
+    """Classe de Modelo para Equações de Navier Stokes, utilizando
+    o modelo SIMPLE para o acoplamento pressão velocidade, e o
+    esquema UDS Central Discretization Scheme para interpolar a velocidade."""
+
+    def build_matrix_v(self, grid):
+        "Classe que constrói a matrz de v* (previsão de velocidade)"
+        cpoints = len(grid.cells)
+        v_matrix = np.zeros((cpoints-1, cpoints-1))
+        
+#        for i in range(5):
+#                print grid.v_r(i),
+
+        for i in range(cpoints-1):
+            A = grid.A(i)
+            A_r = grid.A(i+1)
+            A_kph = self.center_scheme(grid.A(i), grid.A(i+1))
+            rho_kph = self.center_scheme(grid.rho(i), grid.rho(i+1))
+            rho = grid.rho(i)
+            rho_r = grid.rho(i+1)
+            mu = grid.mu(i)
+            mu_r = grid.mu(i+1)
+            dx = grid.dx(i)
+            dx_r = grid.dx(i+1)
+            v = self.center_scheme(grid.v_r(i-1), grid.v_r(i))
+            v_r = self.center_scheme(grid.v_r(i), grid.v_r(i+1))
+
+            ac_k = A*rho*v
+            ad_k = (A*mu)/dx
+            ac_kp1 = A_r*rho_r*v_r
+            ad_kp1 = A_r*mu_r/dx_r
+
+            ##  Velocidades Positivas  ##
+            if (grid.v_r(i) >= 0):
+                a_vl = -ac_k - ad_k
+                a_vc = ac_kp1 + ad_kp1 + ad_k
+                a_vr = -ad_kp1
+
+            ##  Velocidades Negativas  ##
+            else:
+                a_vl = -ad_k
+                a_vc = ad_kp1 - ac_k + ad_k
+                a_vr = ac_kp1 - ad_kp1
+
+
+            #Filling Matrix
+            if i == 0:
+                v_BC = v*A_kph/A
+#                v_matrix[i][i] =  -a_vl*A_kph/A + 0.5*v_BC*rho*A*(A_kph/A)**2 + ac_kp1 + ad_k +ad_kp1
+                v_matrix[i][i] =  0.5*v_BC*rho*A*(A_kph/A)**2 + ac_kp1 + ad_k +ad_kp1
+                v_matrix[i][i+1] = a_vr
+            elif i == cpoints-2:
+                v_matrix[i][i-1] = a_vl
+#                v_matrix[i][i] = a_vc #- a_vr*A_rh/A_lh
+                v_matrix[i][i] = ad_k + A_kph*rho_kph*grid.v_r(i)
+                print 
+            else:
+                v_matrix[i][i-1] = a_vl
+                v_matrix[i][i] = a_vc
+                v_matrix[i][i+1] = a_vr
+
+        return v_matrix
+
+    def build_coef_vector_v(self, grid):
+        #classe que constrói o vetor que será resolvido pelo solver
+        n = len(grid.cells)
+        b = np.zeros(n-1)
+
+        for i in range(n-1):
+            A_c = self.center_scheme(grid.A(i), grid.A(i+1))
+            b[i] = -A_c*(grid.p(i+1)-grid.p(i))
+        
+        #Condicao de Contorno a Esquerda
+        A = grid.A(0)
+        A_kph = self.center_scheme(grid.A(0), grid.A(1))
+        mu = grid.mu(0)
+        rho = grid.rho(0)
+        dx = grid.dx(0)
+        v = self.center_scheme(grid.v_r(-1), grid.v_r(0))
+        v_BC = v*A_kph/A
+        ac_k = A*rho*v_BC
+        ad_k = (A*mu)/dx
+        b[0] = (ac_k + ad_k)*v*A_kph/A -A_kph*(grid.p(1)-10)
+        
+        #Condicao de Contorno a Direita
+        A_kph = self.center_scheme(grid.A(n-2), grid.A(n-1))
+        
+        b[n-2] = -A_kph*(0-grid.p(n-2))
+        
+        return b
+
+    #Funções de Interpolação
+    def center_scheme(self, leftprop, rightprop):
+        "Classe interpola utilizando diferencas centrais."
+        return (rightprop + leftprop)/2
+
+if __name__ == '__main__':
+
+    from GridFluid import *
+
+    gridteste = GridFluid(10, 0, 1)
+    for i in range(5):
+        A = 0.5-0.1*(i)
+        Ai = 0.45 -0.1*i
+        gridteste.add_cell(A=A, dx=0.5, rho=1, mu=0, v_r=1/Ai, p=10-2.5*(i))
+
+    modelCD = Model_SIMPLE_UDS()
+
+    A = modelCD.build_matrix_v(gridteste)
+    b = modelCD.build_coef_vector_v(gridteste)
+    print "\nA:\n", A
+    print "\nb:\n", b
+    print "\nv:\n", (np.matrix(A).I*np.matrix(b).T).A1
+    print
+
