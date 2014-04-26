584a356f30d85d4a549726185b07ccfe6f7559c4
Bismarck Gomes Souza Jr <bismarckgomes@gmail.com>
2014-04-26 12:13:12 -0300

    Iniciando a classe Phase

---
+++ Phase.py
@@ -0,0 +1,14 @@
+# -*- coding: iso-8859-1 -*-
+"""
+@author: Bismarck Gomes Souza Jr e Wagner Queiroz Barros
+@date:   Sat Apr 26 12:08:00 2014
+@email:  bismarckgomes@gmail.com e wagnerqb@gmail.com
+@brief:  Classe que representa uma fase no escoamento.
+"""
+
+
+class Phase():
+    "Classe que representa uma fase."
+
+    def __init__(self):
+        pass
